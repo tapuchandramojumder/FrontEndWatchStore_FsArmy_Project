121e3b9a46d80db670e686090042d6801ff70ee9
tapuchandramojumder <mojumdertapu@gmail.com>
2022-10-26 00:55:35 +0600

    new branch created

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,13 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
+  
       {/* <Head>
         <title>Watch Store #Team-F</title>
         <link rel="icon" href="/watch-favicon.ico" />
       </Head> */}
 
-      <h1> Hello Watch Store </h1>
+      <h1> Hello tapu vai </h1>
     </>
   );
 }
